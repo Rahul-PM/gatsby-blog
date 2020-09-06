97af8cd61ecc90ab80f02dc3ea68aea418b74413
Rahul P M <rahul.p@thinkpalm.com>
2020-09-06 13:06:04 +0530

    added a dynamic blog website using gatsby

---
+++ src/pages/page-3.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>HELLO FROM PAGE 3</h1>
+  </Layout>
+)
+
+export default ThirdPage
